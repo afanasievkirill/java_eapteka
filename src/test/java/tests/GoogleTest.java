225e2188473286664092941cb76122366b6f7adc
k.afanaciev <k.afanaciev@eapteka.ru>
2020-07-27 09:26:45 +0300

    first commit

---
+++ src/test/java/tests/GoogleTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class GoogleTest {
+    @Test
+    void selenideSearchTest() {
+        // Открыть google
+        open("https://google.com");
+
+        // Ввести Selenide в поиск
+        $(byName("q")).setValue("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("html").shouldHave(text("ru.selenide.org"));
+    }
+}
