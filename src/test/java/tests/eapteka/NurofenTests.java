d4184b5bf18baf4869b2125a30b6e1a3289f5208
Afanaciev Kirill <afanasiev.kirill@gmail.com>
2020-08-03 22:23:51 +0300

    @samesmile first

--- src/test/java/tests/eapteka/NurofenTests.java
+++ src/test/java/tests/eapteka/NurofenTests.java
@@ -9,14 +9,20 @@ public class NurofenTests extends BaseTest {
 
     @Test
     void findNurofen(){
+        //Логин
         entrance.shouldHave(text("Вход")).click();
         setLogin.setValue("coxoyis908@in4mail.net");
         setPassword.setValue("password").pressEnter();
         entrance.shouldHave(text("Личный кабинет"));
-
+        //Поиск
         String name = "Афобазол";
         searchBar.setValue(name);
         searchSubbmit.click();
         $x(".//strong[text()="+name+"]");
+        //Добавление первого элемента в корзину (Без проверки)
+        $x(".//*[@class='cc-item--title']").shouldHave(text(name));
+        $x(".//*[@class='cc-item--title']").click();
+        //Выход
+        logout.shouldHave(text("Выход")).click();
     }
 }
