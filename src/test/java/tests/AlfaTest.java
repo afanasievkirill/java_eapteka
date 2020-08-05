bbb69dc4d45c4de17c9ea04ccc3a19f8c7484e75
k.afanaciev <k.afanaciev@eapteka.ru>
2020-08-05 12:07:11 +0300

    add alfa tests

--- src/test/java/tests/AlfaTest.java
+++ src/test/java/tests/AlfaTest.java
@@ -1,21 +1,33 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.conditions.Text;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AlfaTest {
 
-    @Test
-    public void archiveDepositsTest(){
-
+    @BeforeEach
+    public void setUp(){
         open("https://alfabank.ru/");
         $x("//*[@title='Вклады']").click();
+    }
+
+    @Test
+    public void archiveDepositsTest(){
         $x("//*[@title='Депозиты']").click();
         $(byText("Архивные депозиты")).scrollTo().click();
         $$x("//*[@class='product-cell__cell-back']").shouldHaveSize(3);
+    }
+
+    @Test
+    public void depositeInsuranseTest(){
+        $x("//*[@class='navigation']/li").sibling(4).$x(".//a").click();
+        $x("//*[@class='frame-head row smooth-static']").shouldHave(text("Страхование вкладов")) ;
     }
 }
