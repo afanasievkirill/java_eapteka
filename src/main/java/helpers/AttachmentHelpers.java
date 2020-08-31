db52f96165c449e6617db84f3c96ee20f1f2f6a3
Afanaciev Kirill <afanasiev.kirill@gmail.com>
2020-08-31 22:27:58 +0300

    add attachment helper

---
+++ src/main/java/helpers/AttachmentHelpers.java
@@ -0,0 +1,27 @@
+package helpers;
+
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class AttachmentHelpers {
+
+    @Attachment(value = "{attachName}", type = "text/plain")
+    public static String attachAsText(String attachName, String message) {
+        return message;
+    }
+
+    @Attachment(value = "{attachName}", type = "image/png")
+    public static byte[] attachScreenshot(String attachName) {
+        return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+
+    @Attachment(value = "Page source", type = "text/plain")
+    public static byte[] attachPageSource() {
+        return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
+    }
+}
--- src/test/java/tests/qa_automation/google_tests/BaseTest.java
+++ src/test/java/tests/qa_automation/google_tests/BaseTest.java
@@ -1,9 +1,16 @@
 package tests.qa_automation.google_tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static helpers.AttachmentHelpers.*;
+import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class BaseTest {
 
@@ -12,5 +19,19 @@ public class BaseTest {
         SelenideLogger.addListener("allure", new AllureSelenide()
                 .savePageSource(true).screenshots(true));
         Configuration.headless = true;
+    }
+
+    @AfterEach
+    @Step("Attachments")
+    public void afterEach(){
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Console logs", getBrowserConsoleLogs());
+
+        closeWebDriver();
+    }
+
+    public static String getBrowserConsoleLogs() {
+        return String.join("\n", Selenide.getWebDriverLogs(BROWSER));
     }
 }
